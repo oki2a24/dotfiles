a083ef8f29c9be51dd0926d24baf542e311f18bd
oki2a24 <oki2a24@oki2a24.com>
2017-08-11 10:40:59 +0900

    シンプルなMacの.vimrcを追加する

---
+++ link.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DOTPATH=${HOME}/dotfiles
+
+echo "\$DOTPATH : ${DOTPATH}"
+echo "Creat dotfile symbolic links."
+for f in .??*
+do
+  # Exclude files
+  [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".gitignore" ]] && continue
+  [[ ${f} = "LICENSE" ]] && continue
+  [[ ${f} = "README.md" ]] && continue
+  [[ ${f} = "_gvimrc" ]] && continue
+  [[ ${f} = "_vimrc" ]] && continue
+  [[ ${f} = "install.sh" ]] && continue
+  [[ ${f} = "mklink.bat" ]] && continue
+
+  ln -sfv ${DOTPATH}/${f} ${HOME}/${f}
+done
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
