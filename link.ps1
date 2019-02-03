242c76d3e0d68f2ad520c6a233c1051371ce4d61
oki2a24 <oki2a24@gmail.com>
2019-02-03 17:42:43 +0900

    feat: Git Bash の高さを調整する

--- link.ps1
+++ link.ps1
@@ -1,3 +1,4 @@
 ﻿New-Item -ItemType SymbolicLink -Path $HOME -Name _vimrc -Value $HOME\dotfiles\_vimrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name _gvimrc -Value $HOME\dotfiles\_gvimrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name .bashrc -Value $HOME\dotfiles\.bashrc_win_git_bash
+New-Item -ItemType SymbolicLink -Path $HOME -Name .minttyrc -Value $HOME\dotfiles\.minttyrc
