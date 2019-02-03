a83f8db167244c4b6d00c8c750769c07acc1a4b7
oki2a24 <oki2a24@gmail.com>
2019-02-03 15:32:25 +0900

    feat: Windows の Git Bash の .bashrc も管理対象に含める

--- link.ps1
+++ link.ps1
@@ -1,2 +1,3 @@
 ﻿New-Item -ItemType SymbolicLink -Path $HOME -Name _vimrc -Value $HOME\dotfiles\_vimrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name _gvimrc -Value $HOME\dotfiles\_gvimrc
+New-Item -ItemType SymbolicLink -Path $HOME -Name .bashrc -Value $HOME\dotfiles\.bashrc_win_git_bash
