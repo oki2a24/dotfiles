a5c9f0639a85cec43006cbbfc246202984b6e9fe
oki2a24 <oki2a24@gmail.com>
2019-02-08 08:30:18 +0900

    feat: Solarized Dark を追加する

--- link.ps1
+++ link.ps1
@@ -2,3 +2,4 @@
 New-Item -ItemType SymbolicLink -Path $HOME -Name _gvimrc -Value $HOME\dotfiles\_gvimrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name .bashrc -Value $HOME\dotfiles\.bashrc_win_git_bash
 New-Item -ItemType SymbolicLink -Path $HOME -Name .minttyrc -Value $HOME\dotfiles\.minttyrc
+New-Item -ItemType SymbolicLink -Path $HOME -Name .mintty -Value $HOME\dotfiles\.mintty
