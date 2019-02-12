9bc96e378bdad4a6fd146d8527bea83caffab4db
oki2a24 <oki2a24@gmail.com>
2019-02-13 07:34:15 +0900

    refactor: link.sh で無視するファイルをソートしただけ

--- link.sh
+++ link.sh
@@ -7,13 +7,13 @@ echo "Creat dotfile symbolic links."
 for f in .??*
 do
   # Exclude files
+  [[ ${f} = ".bashrc_win_git_bash" ]] && continue
   [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".minttyrc" ]] && continue
   [[ ${f} = "LICENSE" ]] && continue
   [[ ${f} = "README.md" ]] && continue
   [[ ${f} = "_gvimrc" ]] && continue
   [[ ${f} = "_vimrc" ]] && continue
-  [[ ${f} = ".bashrc_win_git_bash" ]] && continue
-  [[ ${f} = ".minttyrc" ]] && continue
   [[ ${f} = "link.ps1" ]] && continue
 
   ln -sfv ${DOTPATH}/${f} ${HOME}/${f}
