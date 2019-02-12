c17bfb4b2d5342fc4f5638ec8cfb90279780d2cb
oki2a24 <oki2a24@gmail.com>
2019-02-13 07:53:50 +0900

    fix: 無視する候補はドットの付くものだけなため、候補とならないファイルをリストから削除する

--- link.sh
+++ link.sh
@@ -6,15 +6,11 @@ echo "\$DOTPATH : ${DOTPATH}"
 echo "Creat dotfile symbolic links."
 for f in .??*
 do
-  # Exclude files
+  # Exclude dot files
   [[ ${f} = ".bashrc_win_git_bash" ]] && continue
   [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".mintty" ]] && continue
   [[ ${f} = ".minttyrc" ]] && continue
-  [[ ${f} = "LICENSE" ]] && continue
-  [[ ${f} = "README.md" ]] && continue
-  [[ ${f} = "_gvimrc" ]] && continue
-  [[ ${f} = "_vimrc" ]] && continue
-  [[ ${f} = "link.ps1" ]] && continue
 
   ln -sfv ${DOTPATH}/${f} ${HOME}/${f}
 done
