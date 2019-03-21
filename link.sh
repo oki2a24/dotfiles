6a4261aaaa9e26c536f8592ef9cc5a46b0f74be0
oki2a24 <oki2a24@gmail.com>
2019-03-22 08:08:40 +0900

    refactor: Vim の設定を一つのディレクトリにまとめる

--- link.sh
+++ link.sh
@@ -8,6 +8,6 @@ echo "Creat dotfile symbolic links."
 ln -sfv ${DOTPATH}/.bash_profile_mac ${HOME}/.bash_profile
 ln -sfv ${DOTPATH}/.gitconfig ${HOME}/.gitconfig
 ln -sfv ${DOTPATH}/.gitignore ${HOME}/.gitignore
-ln -sfv ${DOTPATH}/.vimrc ${HOME}/.vimrc
+ln -sfv ${DOTPATH}/vim/.vimrc ${HOME}/.vimrc
 
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
