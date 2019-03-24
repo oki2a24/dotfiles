d61d97fbf8879d997798df2de8acb646ba1ef588
oki2a24 <oki2a24@gmail.com>
2019-03-25 07:38:48 +0900

    feat: Windows セットアップ時に、_ 始まりの Vim 設定ファイルは使用しないようにする

--- link.ps1
+++ link.ps1
@@ -1,5 +1,4 @@
-﻿New-Item -ItemType SymbolicLink -Path $HOME -Name _vimrc -Value $HOME\dotfiles\vim\_vimrc
-New-Item -ItemType SymbolicLink -Path $HOME -Name _gvimrc -Value $HOME\dotfiles\vim\_gvimrc
+﻿New-Item -ItemType SymbolicLink -Path $HOME -Name .vimrc -Value $HOME\dotfiles\vim\.vimrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name .bashrc -Value $HOME\dotfiles\.bashrc_win_git_bash
 New-Item -ItemType SymbolicLink -Path $HOME -Name .minttyrc -Value $HOME\dotfiles\.minttyrc
 New-Item -ItemType SymbolicLink -Path $HOME -Name .mintty -Value $HOME\dotfiles\.mintty
