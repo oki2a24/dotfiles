2e1eb46721546419a1f6b213fbdc63c711a2b9cf
oki2a24 <oki2a24@gmail.com>
2019-03-25 07:52:49 +0900

    refactor: Git の設定を 1 つのディレクトリにまとめる

--- link.sh
+++ link.sh
@@ -6,8 +6,8 @@ echo "\$DOTPATH : ${DOTPATH}"
 echo "Creat dotfile symbolic links."
 
 ln -sfv ${DOTPATH}/.bash_profile_mac ${HOME}/.bash_profile
-ln -sfv ${DOTPATH}/.gitconfig ${HOME}/.gitconfig
-ln -sfv ${DOTPATH}/.gitignore ${HOME}/.gitignore
+ln -sfv ${DOTPATH}/git/.gitconfig ${HOME}/.gitconfig
+ln -sfv ${DOTPATH}/git/.gitignore ${HOME}/.gitignore
 ln -sfv ${DOTPATH}/vim/.vimrc ${HOME}/.vimrc
 
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
