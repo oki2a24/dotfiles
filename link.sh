acc6c68865f3cd31766f9234488feffe7513fe29
oki2a24 <oki2a24@gmail.com>
2019-03-26 07:00:30 +0900

    rafactor: bash 設定を1つのディレクトリにまとめる

--- link.sh
+++ link.sh
@@ -5,7 +5,7 @@ readonly DOTPATH=${HOME}/dotfiles
 echo "\$DOTPATH : ${DOTPATH}"
 echo "Creat dotfile symbolic links."
 
-ln -sfv ${DOTPATH}/.bash_profile_mac ${HOME}/.bash_profile
+ln -sfv ${DOTPATH}/bash/.bash_profile_mac ${HOME}/.bash_profile
 ln -sfv ${DOTPATH}/git/.gitconfig ${HOME}/.gitconfig
 ln -sfv ${DOTPATH}/git/.gitignore ${HOME}/.gitignore
 ln -sfv ${DOTPATH}/vim/.vimrc ${HOME}/.vimrc
