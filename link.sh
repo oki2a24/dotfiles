94f5ec61d3db55fdcfa6840ea2e19736e458149a
oki2a24 <oki2a24@gmail.com>
2019-04-09 08:47:04 +0900

    feat: .tmux.conf をバッシュのリンクはるファイルに追加する

--- link.sh
+++ link.sh
@@ -8,6 +8,7 @@ echo "Creat dotfile symbolic links."
 ln -sfv ${DOTPATH}/bash/.bash_profile_mac ${HOME}/.bash_profile
 ln -sfv ${DOTPATH}/git/.gitconfig ${HOME}/.gitconfig
 ln -sfv ${DOTPATH}/git/.gitignore ${HOME}/.gitignore
+ln -sfv ${DOTPATH}/tmux/.tmux.conf ${HOME}/.tmux.conf
 ln -sfv ${DOTPATH}/vim/.vimrc ${HOME}/.vimrc
 
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
