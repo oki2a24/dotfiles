20a71a44962b1a0fa68095fd4c0663bbdb0ef51f
oki2a24 <oki2a24@gmail.com>
2019-04-16 08:01:14 +0900

    feat: tpm を導入する

--- link.sh
+++ link.sh
@@ -11,4 +11,9 @@ ln -sfv ${DOTPATH}/git/.gitignore ${HOME}/.gitignore
 ln -sfv ${DOTPATH}/tmux/.tmux.conf ${HOME}/.tmux.conf
 ln -sfv ${DOTPATH}/vim/.vimrc ${HOME}/.vimrc
 
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+  echo "Clone TPM."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
