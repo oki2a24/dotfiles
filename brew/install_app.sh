9320b5bed1d67f7cfe876f71d3a8e99fd4e80408
oki2a24 <oki2a24@gmail.com>
2019-09-11 23:30:13 +0900

    refacotr: brew で追加した bash_profile は dotfiles と重複するので除去する

--- brew/install_app.sh
+++ brew/install_app.sh
@@ -13,8 +13,5 @@ set -eux
 readonly CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 brew bundle install --file=${CURRENT_DIR}/bundle/mac/Brewfile
-bash "${CURRENT_DIR}/set_bash_completion.sh"
-bash "${CURRENT_DIR}/set_composer.sh"
 bash "${CURRENT_DIR}/set_docker_bash_completion.sh"
 bash "${CURRENT_DIR}/set_fzf.sh"
-bash "${CURRENT_DIR}/set_git_completion.sh"
