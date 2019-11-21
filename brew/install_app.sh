8d65ae0b310598fa05cf33a898e1dff88ddfb101
oki2a24 <oki2a24@gmail.com>
2019-11-22 07:40:23 +0900

    feat: Zsh の Docker 補完セットアップを追加する

--- brew/install_app.sh
+++ brew/install_app.sh
@@ -14,4 +14,5 @@ readonly CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 brew bundle install --file=${CURRENT_DIR}/bundle/mac/Brewfile
 bash "${CURRENT_DIR}/set_docker_bash_completion.sh"
+bash "${CURRENT_DIR}/set_docker_zsh_completion.sh"
 bash "${CURRENT_DIR}/set_fzf.sh"
