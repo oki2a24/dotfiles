26c304ac7d05f90c8365a237969fbb3262f74d10
oki2a24 <oki2a24@gmail.com>
2026-06-13 20:57:44 +0900

    feat: Hermes Agent 用のソフトウェア開発profileの最低限のファイルを追加する

--- link.sh
+++ link.sh
@@ -19,6 +19,8 @@ ln -sfv ${DOTPATH}/zsh/.zshrc ${HOME}/.zshrc
 mkdir -p ${HOME}/.gemini/
 ln -sfv ${DOTPATH}/.gemini/GEMINI.md ${HOME}/.gemini/GEMINI.md
 ln -sfv ${DOTPATH}/.gemini/settings.json ${HOME}/.gemini/settings.json
+mkdir -p ${HOME}/.hermes/profiles/
+ln -sfv ${DOTPATH}/.hermes_profiles/software-engineer ${HOME}/.hermes/profiles/software-engineer
 mkdir -p ${HOME}/.config/zellij/
 ln -sfv ${DOTPATH}/zellij/config.kdl ${HOME}/.config/zellij/config.kdl
 
